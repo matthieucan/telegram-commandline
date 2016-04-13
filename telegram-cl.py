a5fc13bb272d01b80df8485484f44c076f722d72
Matthieu Caneill <matthieu.caneill@imag.fr>
2016-04-13 20:55:25 +0200

    timeouts after 5 seconds

--- telegram-cl.py
+++ telegram-cl.py
@@ -3,6 +3,21 @@
 import telepot
 import argparse
 
+# class Timeout is stolen without shame from
+# http://stackoverflow.com/questions/2281850/timeout-function-if-it-takes-too-long-to-finish
+
+class Timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
 def _get_bot(key):
     """ Returns a bot initiated with an API token. """
     bot = telepot.Bot(key)
@@ -60,7 +75,8 @@ def main():
                        type=int)
 
     args = parser.parse_args()
-    args.func(args)
+    with Timeout(seconds=5):
+        args.func(args)
 
 if __name__ == "__main__":
     main()
